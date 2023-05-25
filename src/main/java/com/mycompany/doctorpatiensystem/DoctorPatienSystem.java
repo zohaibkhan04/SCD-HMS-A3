2b735a313c164fd5515c0d9f8b3dc0a8f14167af
Habib Sajjad <>
2023-05-25 22:11:27 +0500

    GUI

---
+++ src/main/java/com/mycompany/doctorpatiensystem/DoctorPatienSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.doctorpatiensystem;
+
+/**
+ *
+ * @author Habib
+ */
+public class DoctorPatienSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
