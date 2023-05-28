fd186a00cd36bb68e5a65e48e74a290ff294eac3
Zohaib khan <81791963+zohaibkhan04@users.noreply.github.com>
2023-05-28 12:17:48 +0500

    CRUD Implementation

---
+++ src/main/java/com/mycompany/doctorpatiensystem/patients.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.doctorpatiensystem;
+
+/**
+ *
+ * @author Zohaib khan
+ */
+public class patients {
+    int patient_id;
+String name;
+String father_name;
+String emailID;
+int contact;
+String address;
+String gender;
+String blood_group;
+String age;
+String info;
+
+    public patients(int patient_id, String name, String father_name, String emailID, int contact, String address, String gender, String blood_group, String age, String info) {
+        this.patient_id = patient_id;
+        this.name = name;
+        this.father_name = father_name;
+        this.emailID = emailID;
+        this.contact = contact;
+        this.address = address;
+        this.gender = gender;
+        this.blood_group = blood_group;
+        this.age = age;
+        this.info = info;
+    }
+
+    public int getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(int patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFather_name() {
+        return father_name;
+    }
+
+    public void setFather_name(String father_name) {
+        this.father_name = father_name;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    
+}
